b142008245f7afad7a906b96176897d4524639cb
Quang C. Huynh <quang@bluematterscience.com>
2023-02-01 16:30:16 -0800

    Add unit tests

---
+++ tests/manual/run-manual-tests.R
@@ -0,0 +1,9 @@
+
+library(RPC)
+library(openMSE)
+
+testthat::test_file("tests/manual/test-LRP-hist.R")
+
+testthat::test_file("tests/manual/test-MP.R")
+
+testthat::test_file("tests/manual/test-MSE.R")
